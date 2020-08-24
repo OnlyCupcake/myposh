e84f26417c51429ee13c18db7f4a7d4fc737c72d
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2020-08-24 13:15:23 +0000

    code fixes

--- app/src/main/java/com/mistersomov/coinjet/MainActivity.kt
+++ app/src/main/java/com/mistersomov/coinjet/MainActivity.kt
@@ -3,4 +3,9 @@ package com.mistersomov.coinjet
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
-import android.content.pm.ActivityInf
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runt
