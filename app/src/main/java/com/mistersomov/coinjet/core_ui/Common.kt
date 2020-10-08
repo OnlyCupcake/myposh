6cdf6aa3b6ec69aeafcfeb11a96a994f07e9a62b
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2020-10-08 09:15:07 +0000

    Add new icons

--- app/src/main/java/com/mistersomov/coinjet/core_ui/Common.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/Common.kt
@@ -6,4 +6,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import
+import androidx.compose.ui.text.font.FontWeight
+import com.mistersomov.coinjet.R
+
+data class CoinJetColor(
+    val primary: Color,
+    val onPrimary: Color,
+    val primaryContainer: C
