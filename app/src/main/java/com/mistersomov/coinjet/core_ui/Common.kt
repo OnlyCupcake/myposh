29c00b7db36544253096b471f6dfda628e249d84
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2020-10-08 08:13:38 +0000

    GUI tweaks

--- app/src/main/java/com/mistersomov/coinjet/core_ui/Common.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/Common.kt
@@ -2,4 +2,8 @@ package com.mistersomov.coinjet.core_ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.staticCompositionLocalOf
-import a
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import
