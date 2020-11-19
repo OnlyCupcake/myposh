7d226aa696fbde60e3ac26cd75afb77cc29e1277
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2020-11-19 05:51:55 +0000

    Additional tests.

--- app/src/main/java/com/mistersomov/coinjet/core_ui/Palette.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/Palette.kt
@@ -3,4 +3,8 @@ package com.mistersomov.coinjet.core_ui
 import androidx.compose.ui.graphics.Color
 
 val baseLightPalette = CoinJetColor(
-    primary = 
+    primary = Color(0xFF3B5BA9),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFFDAE2FF),
+    onPrimaryContainer = Color(0xFF001848),
+    secondary = Color(0x
