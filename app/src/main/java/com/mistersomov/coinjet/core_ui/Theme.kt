245dff84d1ee73ab68ddb77701899c7c97c4b3b0
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2020-12-24 22:58:48 +0000

    Updated INSTALL file

--- app/src/main/java/com/mistersomov/coinjet/core_ui/Theme.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/Theme.kt
@@ -1,4 +1,7 @@
 package com.mistersomov.coinjet.core_ui
 
 import androidx.compose.runtime.Composable
-im
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.
