80a90e7ca784261172c400d81cc2282abcb3f26b
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-01-04 10:12:51 +0000

    merge from tridge

--- app/src/main/java/com/mistersomov/coinjet/core_ui/Theme.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/Theme.kt
@@ -4,4 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MainTheme(
+    isDarkTheme: Boolean = false, content: @Composable () -> Unit
+) {
+    val colors = when (isDarkTheme) {
+        true -> baseDarkPalette
