7bbe2d0cd1faf4864b656d204eba592ee0c2c81a
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-03-15 10:50:45 +0000

    fix linter errors

--- app/src/main/java/com/mistersomov/coinjet/core_ui/effect/AnimatedColor.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/effect/AnimatedColor.kt
@@ -3,4 +3,9 @@ package com.mistersomov.coinjet.core_ui.effect
 import androidx.compose.animation.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.runtime.*
-import an
+import androidx.compose.ui.graphics.Color
+import com.mistersomov.coinjet.core_ui.CoinJetTheme
+
+@Composable
+fun animateDigitColor(digit: Double, initialColor: Color): Color {
+    val prev
