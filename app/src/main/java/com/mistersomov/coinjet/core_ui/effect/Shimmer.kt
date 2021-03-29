a03798707a3a3af6103184496c09c320b4235039
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-03-29 22:24:49 +0000

    Update posts.html

--- app/src/main/java/com/mistersomov/coinjet/core_ui/effect/Shimmer.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/effect/Shimmer.kt
@@ -2,4 +2,8 @@ package com.mistersomov.coinjet.core_ui.effect
 
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androi
