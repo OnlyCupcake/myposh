a8bf1091b4e0e0a241f9a00af47412acecfe1218
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-05-05 17:15:43 +0000

    Update basic.js

--- app/src/main/java/com/mistersomov/coinjet/core_ui/effect/Swipe.kt
+++ app/src/main/java/com/mistersomov/coinjet/core_ui/effect/Swipe.kt
@@ -4,3 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.
