8018e14619b1973f55453b78e40b3d804dc192d3
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-06-27 19:07:30 +0000

    Update button styles

--- app/src/main/java/com/mistersomov/coinjet/data/database/CoinjetDatabase.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/database/CoinjetDatabase.kt
@@ -4,4 +4,10 @@ import androidx.room.*
 import com.mistersomov.coinjet.data.database.dao.CoinDao
 import com.mistersomov.coinjet.data.database.dao.SearchCoinDao
 import com.mistersomov.coinjet.data.database.entity.CoinEntity
-import 
+import com.mistersomov.coinjet.data.database.entity.SearchCoinEntity
+
+@Database(
+    entities = [CoinEntity::class, SearchCoinEntity::class],
+    version = 1,
+)
+abstract class CoinjetDatabas
