aded18c35b6b5aac7ef4573e8e2b00e544387e94
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-07-01 14:15:10 +0000

    automated_serno

--- app/src/main/java/com/mistersomov/coinjet/data/database/entity/CoinEntity.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/database/entity/CoinEntity.kt
@@ -1,1 +1,7 @@
-package com.misterso
+package com.mistersomov.coinjet.data.database.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = CoinEntity.TABLE_COIN_NAME)
+data clas
