a86a0b62e47b047f9c4c014981c5d6f982b7d47a
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-07-02 12:25:18 +0000

    woo

--- app/src/main/java/com/mistersomov/coinjet/data/database/entity/CoinEntity.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/database/entity/CoinEntity.kt
@@ -4,4 +4,11 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = CoinEntity.TABLE_COIN_NAME)
-data clas
+data class CoinEntity(
+    @PrimaryKey
+    val id: String,
+    val symbol: String,
+    val fullName: String,
+    val fromSymbol: String,
+    val toSymbol: String,
+    val 
