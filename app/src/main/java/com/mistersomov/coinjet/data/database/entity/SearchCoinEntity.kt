2d66343b79faabdeb96ba7629458bdf149bd504a
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-07-04 17:47:57 +0000

    nts

--- app/src/main/java/com/mistersomov/coinjet/data/database/entity/SearchCoinEntity.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/database/entity/SearchCoinEntity.kt
@@ -3,4 +3,10 @@ package com.mistersomov.coinjet.data.database.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = SearchCoinEnti
+@Entity(tableName = SearchCoinEntity.TABLE_SEARCH_COIN_NAME)
+data class SearchCoinEntity(
+    @PrimaryKey
+    val id: String,
+    val symbol: String,
+    val fullName: String,
+  
