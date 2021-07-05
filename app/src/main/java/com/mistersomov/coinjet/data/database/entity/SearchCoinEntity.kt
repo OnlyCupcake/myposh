7600c8095bb7404f64110a98e5dc30849a04dfb5
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-07-05 07:05:47 +0000

    drm/gma500: Increase max resolution for mode setting

--- app/src/main/java/com/mistersomov/coinjet/data/database/entity/SearchCoinEntity.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/database/entity/SearchCoinEntity.kt
@@ -9,4 +9,10 @@ data class SearchCoinEntity(
     val id: String,
     val symbol: String,
     val fullName: String,
-  
+    val fromSymbol: String,
+    val toSymbol: String,
+    val price: Double,
+    val lastUpdate: String,
+    val volume24Hour: Double,
+    val volume24hourTo: Double,
+    val ope
