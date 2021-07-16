54b8375b7afcaca1fd7051adcfed13aa7c9cbbcf
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-07-16 08:56:28 +0000

    basics

--- app/src/main/java/com/mistersomov/coinjet/data/network/api/CoinApiService.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/network/api/CoinApiService.kt
@@ -1,3 +1,6 @@
 package com.mistersomov.coinjet.data.network.api
 
-import com.misterso
+import com.mistersomov.coinjet.data.network.model.CoinListDto
+import com.mistersomov.coinjet.data.network.model.QuoteJsonContainerDto
+import retrofit2.http.GET
+import retrofit2.h
