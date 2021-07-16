f67317f131ac4f509e050743eae2169623b35bd5
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-07-16 22:26:57 +0000

    Added missing license

--- app/src/main/java/com/mistersomov/coinjet/data/network/api/CoinApiService.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/network/api/CoinApiService.kt
@@ -3,4 +3,10 @@ package com.mistersomov.coinjet.data.network.api
 import com.mistersomov.coinjet.data.network.model.CoinListDto
 import com.mistersomov.coinjet.data.network.model.QuoteJsonContainerDto
 import retrofit2.http.GET
-import retrofit2.h
+import retrofit2.http.Query
+
+interface CoinApiService {
+
+    @GET(ENDPOINT_COIN_LIST)
+    suspend fun getTopCoinList(
+        @Query(QUERY_PARAM_LIMIT) limit: Int = QUERY_PARAM_LIMI
