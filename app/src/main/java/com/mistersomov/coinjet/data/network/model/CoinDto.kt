79a5e700f15228f349d2a5bc47ad44843a05c91c
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-07-30 10:24:16 +0000

    Bump to 1.1.3

--- app/src/main/java/com/mistersomov/coinjet/data/network/model/CoinDto.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/network/model/CoinDto.kt
@@ -1,11 +1,7 @@
-
 package com.mistersomov.coinjet.data.network.model
 
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class CoinContainerDto(
-    @SerializedName("CoinInfo")
-    @Expose
-    val coin: CoinDto? = null,
-)
+data class CoinDto(
+    @SerializedName(
