8f248096458f15beed52f3b3b60af0b497481cf5
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-08-13 06:11:20 +0000

    Added ignore files

--- app/src/main/java/com/mistersomov/coinjet/data/network/model/QuoteDto.kt
+++ app/src/main/java/com/mistersomov/coinjet/data/network/model/QuoteDto.kt
@@ -3,8 +3,4 @@ package com.mistersomov.coinjet.data.network.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class CoinListDto(
-    @SerializedName("Data")
-    @Expose
-    val coins: List<CoinContainerDto>? = null,
-)
+da
