e9d7d287b4f82bf35a14ea64aedeb260f2823e33
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-10-02 21:04:39 +0000

    Move function

--- app/src/main/java/com/mistersomov/coinjet/di/ServiceModule.kt
+++ app/src/main/java/com/mistersomov/coinjet/di/ServiceModule.kt
@@ -3,4 +3,9 @@ package com.mistersomov.coinjet.di
 import com.mistersomov.coinjet.BuildConfig
 import com.mistersomov.coinjet.data.network.interceptor.CoinProtocolInterceptor
 import com.mistersomov.coinjet.data.service.CoinService
-import com.mistersomov.coinjet.data.service.CoinSe
+import com.mistersomov.coinjet.data.service.CoinServiceImpl
+import com.mistersomov.coinjet.utils.addInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponen
