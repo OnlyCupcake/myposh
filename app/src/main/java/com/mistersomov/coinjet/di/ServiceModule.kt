cd039efcacfb4c99ddbb48a88ad3511dc637a805
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2021-10-04 10:05:30 +0000

    added editor config

--- app/src/main/java/com/mistersomov/coinjet/di/ServiceModule.kt
+++ app/src/main/java/com/mistersomov/coinjet/di/ServiceModule.kt
@@ -13,4 +13,12 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Singleton
+    @Provides
+    fun provideOkkHttp(interceptors: MutableSet<
