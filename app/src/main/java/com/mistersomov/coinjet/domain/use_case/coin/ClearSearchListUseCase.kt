9ce6e183daf3b94b016cd5b8e86f1a5faeb1fa22
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-01-04 22:57:28 +0000

    [Modify] pod spec

--- app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/ClearSearchListUseCase.kt
+++ app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/ClearSearchListUseCase.kt
@@ -3,4 +3,9 @@ package com.mistersomov.coinjet.domain.use_case.coin
 import com.mistersomov.coinjet.di.qualifier.DefaultDispatcher
 import com.mistersomov.coinjet.domain.repository.CoinRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.cor
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class ClearSearchListUseCase @Inject constructor(
+    private val repository: CoinRepository,
+    @DefaultDispatcher p
