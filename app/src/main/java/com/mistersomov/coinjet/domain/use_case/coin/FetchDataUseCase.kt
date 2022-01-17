08e5c8adb922bf3b7451de904b51cfa4e85e7678
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-01-17 18:55:43 +0000

    fixing template

--- app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/FetchDataUseCase.kt
+++ app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/FetchDataUseCase.kt
@@ -5,4 +5,9 @@ import com.mistersomov.coinjet.domain.model.Coin
 import com.mistersomov.coinjet.domain.repository.CoinRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.f
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class FetchDataUseCase @Inject constructor(
+    private val repository: CoinRepository,
+    @DefaultDispatcher private val default
