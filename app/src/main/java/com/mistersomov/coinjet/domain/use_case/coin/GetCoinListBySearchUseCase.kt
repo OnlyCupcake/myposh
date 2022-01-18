597db1ecfdef84ea2068a96764fdd05f70ff3bdf
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-01-18 11:08:17 +0000

    Updated russian translation.

--- app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/GetCoinListBySearchUseCase.kt
+++ app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/GetCoinListBySearchUseCase.kt
@@ -6,3 +6,22 @@ import com.mistersomov.coinjet.domain.model.Coin
 import com.mistersomov.coinjet.domain.repository.CoinRepository
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetCoinListBySearchUseCase @Inject constructor(
+    private val repository: CoinRepository,
+    @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher,
+) {
+    suspend operator fun invoke(query: String): List<Coin> {
+        return withContext(defaultDispatcher)  {
+            when {
+                query[0] == '#' -> {
+                    val formattedQuery = query.trim { it == '#' }
+
+                    repository.getCoinListBySymbol(formattedQuery)
+                }
+                else -> repository.getCoinListByName(query.trimIndent())
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/GetRecentSearchListUseCase.kt
@@ -0,0 +1,4 @@
+package com.mistersomov.coinjet.domain.use_case.coin
+
+import com.mistersomov.coinjet.di.qualifier.DefaultDispatcher
+import com.mistersomov.coinjet.domai
