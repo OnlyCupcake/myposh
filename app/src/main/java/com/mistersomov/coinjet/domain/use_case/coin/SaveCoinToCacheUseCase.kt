ade16cae61108f4034699704179873c19488c2ef
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-02-12 08:34:24 +0000

    Adjust spacing

--- app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/SaveCoinToCacheUseCase.kt
+++ app/src/main/java/com/mistersomov/coinjet/domain/use_case/coin/SaveCoinToCacheUseCase.kt
@@ -1,4 +1,8 @@
 package com.mistersomov.coinjet.domain.use_case.coin
 
 import com.mistersomov.coinjet.di.qualifier.DefaultDispatcher
-import com.mistersomov.coinjet.domain.model.
+import com.mistersomov.coinjet.domain.model.Coin
+import com.mistersomov.coinjet.domain.repository.CoinRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import 
