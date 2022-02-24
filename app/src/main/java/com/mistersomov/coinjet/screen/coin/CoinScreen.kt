9a870b99aa0c978730a94cbed07f50c90dcd257a
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-02-24 16:54:59 +0000

    Translated by lars

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/CoinScreen.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/CoinScreen.kt
@@ -2,4 +2,7 @@ package com.mistersomov.coinjet.screen.coin
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
-import an
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Bac
