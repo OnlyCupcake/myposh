8ee6765391bed366923514a9187428a6d4a7cda9
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-02-26 16:33:17 +0000

    removed unnecessary comments

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/CoinScreen.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/CoinScreen.kt
@@ -16,4 +16,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.r
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.mistersomov.coinjet.R
+import c
