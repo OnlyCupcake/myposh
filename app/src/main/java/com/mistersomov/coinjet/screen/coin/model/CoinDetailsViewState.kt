9a11383ca879343594d813d1098032c75aacd0bc
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-05-14 07:26:44 +0000

    0.3.3

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/model/CoinDetailsViewState.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/model/CoinDetailsViewState.kt
@@ -4,4 +4,9 @@ import com.mistersomov.coinjet.domain.model.Coin
 
 sealed class CoinDetailsViewState {
 
-   
+    object Hide : CoinDetailsViewState()
+
+    data class SimpleDetails(val coin: Coin) : CoinDetailsViewState()
+
+    data class FullDetails(val coin: Coin) : CoinDetailsViewState()
+}
