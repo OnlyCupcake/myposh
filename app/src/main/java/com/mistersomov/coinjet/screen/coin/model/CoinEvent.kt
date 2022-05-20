bbbed774eaf428dbe79f95ea962ba9e59e212e3c
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-05-20 19:20:20 +0000

    fixed newline

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/model/CoinEvent.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/model/CoinEvent.kt
@@ -1,1 +1,8 @@
-package c
+package com.mistersomov.coinjet.screen.coin.model
+
+sealed class CoinEvent {
+
+    object FetchData : CoinEvent()
+
+    data class Click(val symbol: String) : CoinEvent()
+}
