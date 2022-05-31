90c79a91dfc294b0bc8cd05adcb3ffed670032cc
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-05-31 08:49:45 +0000

    add typescript

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/model/SearchEvent.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/model/SearchEvent.kt
@@ -1,1 +1,11 @@
-package com.mistersomov.coinjet.screen.coin.mo
+package com.mistersomov.coinjet.screen.coin.model
+
+import com.mistersomov.coinjet.domain.model.Coin
+
+sealed class SearchEvent {
+
+    object Hide : SearchEvent()
+
+    object ShowRecentSearch : SearchEvent()
+
+    data class La
