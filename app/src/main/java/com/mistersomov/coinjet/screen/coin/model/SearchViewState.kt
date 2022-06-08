643d636cffd459c85461f8b282a551c73363e95c
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-06-08 08:07:50 +0000

    better style

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/model/SearchViewState.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/model/SearchViewState.kt
@@ -1,1 +1,9 @@
-package com.m
+package com.mistersomov.coinjet.screen.coin.model
+
+import com.mistersomov.coinjet.domain.model.Coin
+
+sealed class SearchViewState {
+
+    object Hide : SearchViewState()
+
+    object FirstSearch : Se
