bb93346f4369158f3dbcdd53264d9b5bbf978e22
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-06-17 13:32:39 +0000

    Bumped version to 0.1.4

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/view/CoinViewLoading.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/view/CoinViewLoading.kt
@@ -1,4 +1,8 @@
 package com.mistersomov.coinjet.screen.coin.view
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyC
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.com
