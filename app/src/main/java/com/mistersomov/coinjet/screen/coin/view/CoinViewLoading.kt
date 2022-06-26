b84e5029e97532cb0e6d72fde80f6e5df43b13b2
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-06-26 17:48:28 +0000

    Hello

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/view/CoinViewLoading.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/view/CoinViewLoading.kt
@@ -5,4 +5,9 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.com
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.mistersomov.coinjet.core_ui.component.ListItem
+impor
