998034ed36df1c9fce694271447e222bafdd9f9c
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-07-19 21:04:27 +0000

    Release 3.0.4

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchCoinDetails.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchCoinDetails.kt
@@ -1,1 +1,6 @@
-package com.mistersomov.coinjet.screen.coin.view.sear
+package com.mistersomov.coinjet.screen.coin.view.search
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.
