dfddcac02ff68b423c4a024f386cf9ac111b767d
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-07-22 10:00:16 +0000

    write readme.

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchCoinDetails.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchCoinDetails.kt
@@ -3,4 +3,9 @@ package com.mistersomov.coinjet.screen.coin.view.search
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+imp
