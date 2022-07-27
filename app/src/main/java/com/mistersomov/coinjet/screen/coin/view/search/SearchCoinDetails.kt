dd919f19b2b651e6ff40a0472c60aa2eaff7a634
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-07-27 15:29:53 +0000

    Version 3.2.1

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchCoinDetails.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchCoinDetails.kt
@@ -8,4 +8,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-imp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import co
