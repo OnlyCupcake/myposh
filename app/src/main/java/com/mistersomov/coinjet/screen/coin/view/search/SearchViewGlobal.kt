8be6c4038340cb67752c39d407db7c96e4845000
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-08-13 06:04:12 +0000

    PM / sleep: Use valid_state() for platform-dependent sleep states only

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchViewGlobal.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchViewGlobal.kt
@@ -2,4 +2,8 @@ package com.mistersomov.coinjet.screen.coin.view.search
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.L
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Divider
+import androidx.compose.material.
