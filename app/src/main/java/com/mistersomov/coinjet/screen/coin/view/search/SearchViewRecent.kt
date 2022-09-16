f3e6f8ffad6d9640a318bfa17f993918077b5970
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-09-16 11:28:07 +0000

    rtc: alarm: Add power-on alarm feature

--- app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchViewRecent.kt
+++ app/src/main/java/com/mistersomov/coinjet/screen/coin/view/search/SearchViewRecent.kt
@@ -4,4 +4,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.mat
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
