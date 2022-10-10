412f13a122ea48cce43625a342394b986d96748a
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-10-10 10:01:24 +0000

    cgroup: notify_on_release may not be triggered in some cases

--- app/src/main/java/com/mistersomov/coinjet/utils/OkHttpExtensions.kt
+++ app/src/main/java/com/mistersomov/coinjet/utils/OkHttpExtensions.kt
@@ -1,1 +1,7 @@
-pac
+package com.mistersomov.coinjet.utils
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+
+fun <T : Any> OkHttpClient.Builder.addInterceptor(
+    interceptors: List<Intercep
