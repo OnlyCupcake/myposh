c3c6a39589f1ab926e5fe18599e355792375131a
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-10-11 20:02:32 +0000

    Version bump to 0.1.4

--- app/src/main/java/com/mistersomov/coinjet/utils/OkHttpExtensions.kt
+++ app/src/main/java/com/mistersomov/coinjet/utils/OkHttpExtensions.kt
@@ -4,4 +4,12 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 
 fun <T : Any> OkHttpClient.Builder.addInterceptor(
-    interceptors: List<Intercep
+    interceptors: List<Interceptor>,
+    interceptorType: Class<T>
+): OkHttpClient.Builder {
+    interceptors
+        .filter { interceptorType.isInstance(it) }
+        .forEach(this::addInterceptor)
+
+    return this
+}
