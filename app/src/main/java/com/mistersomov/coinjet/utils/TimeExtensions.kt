18b5fe552f24ea7d6a29f2bb03cfad8482b626b2
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2022-10-18 08:16:45 +0000

    gradle build

--- app/src/main/java/com/mistersomov/coinjet/utils/TimeExtensions.kt
+++ app/src/main/java/com/mistersomov/coinjet/utils/TimeExtensions.kt
@@ -1,1 +1,8 @@
-package 
+package com.mistersomov.coinjet.utils
+
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import java.sql.Timestamp
+
+fun Long?.convertTime(): String {
+    retu
