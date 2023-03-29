20e28113a616aab78be7520d12c2e54b3cf66cc6
OnlyCupcake <98707028+OnlyCupcake@users.noreply.github.com>
2023-03-29 22:37:19 +0000

    update subrepos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,9 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.startsWith("com.android")) {
-                useModule("com.android.tools.build:gradle:7.3
+                useModule("com.android.tools.build:gradle:7.3.1")
+            }
+            if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
+                useVersion("1.7.20")
+            }
+            if (requested.id.id.startsWith("d
